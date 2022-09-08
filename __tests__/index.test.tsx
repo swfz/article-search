4573ac7934406c861452cf22987d4009d487dc48
swfz <sawafuji.09@gmail.com>
2022-09-09 04:05:09 +0900

    test: #18 first test

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,17 @@
+import { render } from "@testing-library/react";
+import Search from "../pages/index";
+import "@testing-library/jest-dom";
+import { act } from "react-dom/test-utils";
+
+describe("Search", () => {
+  it("renders a heading", async () => {
+    const { getByRole } = render(<Search />);
+
+    await act(async () => new Promise((r) => setTimeout(r, 500)));
+    const heading = getByRole("heading", {
+      name: /Article Search/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
