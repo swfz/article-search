9d71bd08f65c4d8a25d2835c2c1ad3ea9507ce81
swfz <sawafuji.09@gmail.com>
2022-09-10 05:30:29 +0900

    chore: format

--- mocks/handler.ts
+++ mocks/handler.ts
@@ -6,7 +6,6 @@ import queryWordsMsw from "./json/search-response-msw.json";
 
 export const handlers = [
   rest.post("https://*.algolia.net/1/indexes/*/queries", (req, res, ctx) => {
-
     const bodyString = req.body as string;
 
     const body = JSON.parse(bodyString);
@@ -14,15 +13,14 @@ export const handlers = [
       ...new URLSearchParams(body.requests[0].params).entries(),
     ].reduce((obj, e) => ({ ...obj, [e[0]]: e[1] }), {} as { query: string });
 
-
-    if (params.query === 'm') {
-      return res(ctx.status(200), ctx.json(queryWordsM))
+    if (params.query === "m") {
+      return res(ctx.status(200), ctx.json(queryWordsM));
     }
-    if (params.query === 'ms') {
-      return res(ctx.status(200), ctx.json(queryWordsMs))
+    if (params.query === "ms") {
+      return res(ctx.status(200), ctx.json(queryWordsMs));
     }
-    if (params.query === 'msw') {
-      return res(ctx.status(200), ctx.json(queryWordsMsw))
+    if (params.query === "msw") {
+      return res(ctx.status(200), ctx.json(queryWordsMsw));
     }
   }),
 ];
