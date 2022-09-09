2459097ef7c6c61b59f5f04fc60160bbfa1aa151
swfz <sawafuji.09@gmail.com>
2022-09-09 19:46:17 +0900

    feat: #18 local開発でmswを使えるように設定した

---
+++ mocks/handler.ts
@@ -0,0 +1,34 @@
+import { rest } from "msw";
+
+import queryWordsM from "./json/search-response-m.json";
+import queryWordsMs from "./json/search-response-ms.json";
+import queryWordsMsw from "./json/search-response-msw.json";
+
+export const handlers = [
+  rest.post("https://*.algolia.net/1/indexes/*/queries", (req, res, ctx) => {
+
+    const wordCountResponseMap = [
+      queryWordsM, // m
+      queryWordsMs, // ms
+      queryWordsMsw, // msw
+    ];
+
+    const bodyString = req.body as string;
+
+    const body = JSON.parse(bodyString);
+    const params = [
+      ...new URLSearchParams(body.requests[0].params).entries(),
+    ].reduce((obj, e) => ({ ...obj, [e[0]]: e[1] }), {} as { query: string });
+
+
+    if (params.query === 'm') {
+      return res(ctx.status(200), ctx.json(queryWordsM))
+    }
+    if (params.query === 'ms') {
+      return res(ctx.status(200), ctx.json(queryWordsMs))
+    }
+    if (params.query === 'msw') {
+      return res(ctx.status(200), ctx.json(queryWordsMsw))
+    }
+  }),
+];
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
+if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
+  console.log('enable');
+
+  require('../mocks')
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
