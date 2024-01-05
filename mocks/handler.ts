e2c04a0dd96b93f7ede29d218d5f59ee8092f315
swfz <sawafuji.09@gmail.com>
2024-01-05 19:40:11 +0900

    feat: Jestが動くようにした

--- mocks/handler.ts
+++ mocks/handler.ts
@@ -1,26 +1,45 @@
-import { rest } from "msw";
+import { HttpResponse, http } from "msw";
 
 import queryWordsM from "./json/search-response-m.json";
 import queryWordsMs from "./json/search-response-ms.json";
 import queryWordsMsw from "./json/search-response-msw.json";
 
 export const handlers = [
-  rest.post("https://*.algolia.net/1/indexes/*/queries", (req, res, ctx) => {
-    const bodyString = req.body as string;
+  http.post(
+    "https://*.algolia.net/1/indexes/*/queries",
+    async ({ request, params }) => {
+      const body = await request.json();
 
-    const body = JSON.parse(bodyString);
-    const params = [
-      ...new URLSearchParams(body.requests[0].params).entries(),
-    ].reduce((obj, e) => ({ ...obj, [e[0]]: e[1] }), {} as { query: string });
+      const isRecords = (
+        obj: any,
+      ): obj is { requests: { indexName: string; params: string }[] } => {
+        return (
+          Array.isArray(obj.requests) &&
+          obj.requests.every((r: any) => r.indexName && r.params)
+        );
+      };
 
-    if (params.query === "m") {
-      return res(ctx.status(200), ctx.json(queryWordsM));
-    }
-    if (params.query === "ms") {
-      return res(ctx.status(200), ctx.json(queryWordsMs));
-    }
-    if (params.query === "msw") {
-      return res(ctx.status(200), ctx.json(queryWordsMsw));
-    }
-  }),
+      if (!isRecords(body)) {
+        return new HttpResponse(JSON.stringify({}));
+      }
+      const entries = [
+        ...new URLSearchParams(body.requests[0].params).entries(),
+      ];
+
+      const queryParams = entries.reduce(
+        (obj, e) => ({ ...obj, [e[0]]: e[1] }),
+        {} as { query: string },
+      );
+
+      if (queryParams.query === "m") {
+        return new HttpResponse(JSON.stringify(queryWordsM));
+      }
+      if (queryParams.query === "ms") {
+        return new HttpResponse(JSON.stringify(queryWordsMs));
+      }
+      if (queryParams.query === "msw") {
+        return new HttpResponse(JSON.stringify(queryWordsMsw));
+      }
+    },
+  ),
 ];
